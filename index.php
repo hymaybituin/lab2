162e54c23fedc15e44817fe3fe5492272bb41e69
hymaybituin <hymaybituin@student.apc.edu.ph>
2024-01-29 22:36:04 +0800

    New Folder

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "My First PHP script!<br>";
+?>
+
+
+<?php
+Echo "Hello World!<br>";
+echo "Hello World!<br>";
+Echo "Hello World!<br>";
+?>
+
+</body>
+</html>
