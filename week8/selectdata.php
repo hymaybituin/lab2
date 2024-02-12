7a210852fba0d35b56c54a25cd7f1dc92fddaf98
hymaybituin <152581582+hymaybituin@users.noreply.github.com>
2024-02-13 01:00:25 +0800

    Update selectdata.php

--- week8/selectdata.php
+++ week8/selectdata.php
@@ -1,4 +1,9 @@
 <?php
+//$servername = "localhost";
+//$username = "root";
+//$password = "";
+//$dbname = "myDB";
+
 $servername = "localhost";
 $username = "webprogmi221";
 $password = "g_6bCitLu.ljMK*m";
@@ -11,17 +16,17 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, name, email, website, comment, gender FROM myguests";
+$sql = "INSERT INTO amtauyan_myguests (name, email, website, comment, gender)
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["email"]. " " . $row["website"]. " " . $row["comment"]. " " . $row["gender"]. "<br>";
+VALUES ('$name', '$email', '$website', '$comment', '$gender')";
 
-  }
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
 } else {
-  echo "0 results";
+  echo "Error: " . $sql . "<br>" . $conn->error;
 }
+
 $conn->close();
+?>
 ?>
